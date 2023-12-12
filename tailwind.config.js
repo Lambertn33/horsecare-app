a9104c775e378583735ff8ad97faf27f827c12da
Lambertn33 <lambertgithub@gmail.com>
2023-12-12 10:24:30 +0200

    feat: flowbite installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+import flowbitePlugin from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
 
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [flowbitePlugin],
+};
